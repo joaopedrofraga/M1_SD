92f659810a23f8697240c295dcfff385b4f3327d
joão fraga <joaofraga60@gmail.com>
2026-09-02 19:13:12 -0300

    criacao receber json

--- src/protocolo.py
+++ src/protocolo.py
@@ -4,3 +4,8 @@ import json
 def criar_mensagem(tipo: str, origem: int, **campos) -> dict:
     return {"tipo": tipo, "origem": origem, **campos}
 
+def receber_json(leitor: asyncio.StreamReader) -> dict:
+    linha = await leitor.readline()
+    if not linha:
+        raise ValueError("Mensagem vazia")
+    return json.loads(linha)
