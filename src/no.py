0ba7f691e66636a8f879f4c6fd229e709ae5adb9
joão fraga <joaofraga60@gmail.com>
2026-09-02 19:36:52 -0300

    inicio da criacao do no

--- src/no.py
+++ src/no.py
@@ -5,3 +5,17 @@ from time import monotonic
 from .configuracao import Configuracao
 from .protocolo import criar_mensagem, enviar_json, receber_json
 from .relogio import RelogioVetorial, pode_entregar
+
+class No:
+    def __init__(self, configuracao: Configuracao, identificador: int,
+                 intervalo_batimento=1, limite_lider=3,
+                 espera_eleicao=1, limite_captura=5, mostrar_saida=True):
+        self.configuracao = configuracao
+        self.identificador = identificador
+        self.dados = configuracao.por_identificador(identificador)
+        self.indices = {numero: posicao for posicao, numero in enumerate(configuracao.identificadores)}
+        self.intervalo_batimento = intervalo_batimento
+        self.limite_lider = limite_lider
+        self.espera_eleicao = espera_eleicao
+        self.limite_captura = limite_captura
+        self.mostrar_saida = mostrar_saida
